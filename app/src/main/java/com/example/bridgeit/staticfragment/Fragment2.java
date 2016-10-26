f104d7b3f4255fd43328718ebc25e3ec6e25d785
jyothishkm <jyothishkm88@gmail.com>
2016-10-26 10:27:44 +0530

    staticFragment

---
+++ app/src/main/java/com/example/bridgeit/staticfragment/Fragment2.java
@@ -0,0 +1,22 @@
+package com.example.bridgeit.staticfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bridgeit on 26/10/16.
+ */
+
+public class Fragment2 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment2,container);
+        return view;
+    }
+}
